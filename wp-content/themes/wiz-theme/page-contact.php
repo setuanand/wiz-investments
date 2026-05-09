4198159a22f086714b8dad9aebdf81dfe061060e
seanand <setu_anand@yahoo.com>
2026-05-08 20:05:04 -0700

    Fix contact form POST redirect to avoid LiteSpeed caching issue

--- wp-content/themes/wiz-theme/page-contact.php
+++ wp-content/themes/wiz-theme/page-contact.php
@@ -2,8 +2,21 @@
 /*
 Template Name: Contact
 */
-$contact_notice = function_exists('wiz_process_contact_submission') ? wiz_process_contact_submission() : '';
+
+$contact_notice = '';
+if (isset($_POST['wiz_contact_submit'])) {
+    $contact_notice = function_exists('wiz_process_contact_submission') ? wiz_process_contact_submission() : '';
+    if (strpos($contact_notice, 'success') !== false) {
+        wp_redirect(home_url('/contact/?sent=1'));
+        exit;
+    }
+}
+
 get_header();
+
+if (isset($_GET['sent'])) {
+    $contact_notice = '<div class="form-notice success">Your message has been sent successfully. We will be in touch soon!</div>';
+}
 ?>
 
 <section class="page-content">
@@ -16,13 +29,13 @@ get_header();
     <?php wp_nonce_field('wiz_contact_action', 'wiz_contact_nonce'); ?>
     <label for="name">Name:</label>
     <input type="text" id="name" name="name" required>
-    
+
     <label for="email">Email:</label>
     <input type="email" id="email" name="email" required>
-    
+
     <label for="message">Message:</label>
     <textarea id="message" name="message" rows="5" required></textarea>
-    
+
     <button type="submit" name="wiz_contact_submit">Send Message</button>
   </form>
   <div class="contact-info">
